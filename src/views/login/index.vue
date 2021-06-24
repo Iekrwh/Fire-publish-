1271f608d822d9e2cd2038fd7f5335f4e15afc25
Iekr <Iekr_wh@qq.com>
2021-06-24 14:21:38 +0800

    second commit

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="login-container">
-    <el-form :model="user" ref="ruleForm" class="login-from">
+       <!-- rules定义表单验证 -->
+    <el-form :model="user" ref="loginForm" class="login-from" :rules="formRules">
       <el-form-item prop="mobile">
         <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
       </el-form-item>
       <el-form-item prop="code">
         <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
       </el-form-item>
-      <el-form-item>
-        <el-checkbox v-model="checked"
+      <el-form-item prop='agree'>
+        <el-checkbox v-model="user.agree"
           >我已阅读并同意用户协议和隐私条款</el-checkbox
         >
       </el-form-item>
-      <el-form-item>
+      <el-form-item >
         <el-button class="login-btn" type="primary" @click="onLogin" :loading="loginLoading"
           >登陆</el-button
         >
@@ -22,7 +23,7 @@
   </div>
 </template>
 <script>
-import requset from '@/utils/request'
+import { login } from '@/api/user'
 
 export default {
   name: 'LoginIndex',
@@ -32,10 +33,35 @@ export default {
     return {
       user: {
         mobile: '', // 手机号
-        code: '' // 验证码
+        code: '', // 验证码
+        agree: false // 是否同意协议
       },
-      checked: false, // 多选款状态
-      loginLoading: false
+      //   checked: false, // 多选款状态
+      loginLoading: false,
+      formRules: { // 表单验证规则配置
+      // required 不能为空   message验证不通过提示  trigger:blur为失去焦点时验证  change为更改后验证
+      // pattern 正则表达  range 使用min max 定义范围    len 要验证字段的长度  enum 枚举
+        mobile: [{ required: true, message: '手机号不能为空', trigger: 'change' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '请输入正确的号码格式', trigger: 'change' }
+        ],
+        code: [{ required: true, message: '验证码不能为空', trigger: 'change' },
+          { min: 6, max: 6, message: '请输入正确的验证码格式', trigger: 'blur' }
+        ],
+        agree: [{
+          validator: (rule, value, callback) => {
+          // 定义验证规则  rule为表单的详细信息  value为布尔
+          // 验证通过: callback()   验证失败:callback(new Error('自定义错误消息提示'))
+            // console.log(value)
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('请同意用户协议'))
+            }
+          },
+          trigger: 'change'
+        }]
+      }
+
     }
   },
   computed: {},
@@ -44,24 +70,32 @@ export default {
   mounted () {},
   methods: {
     onLogin () {
+      this.$refs.loginForm.validate((valid, err) => {
+        // 返回两个参数  valid 一个布尔值是否通过验证   err 验证的具体配置
+        // 所以我们可以通过返回的布尔值来判断表单是否通过验证
+        if (!valid) {
+          // 不通过返回
+          return
+        }
+        // 通过调用api接口验证用户信息
+        this.login()
+      })
+    },
+    login () {
       // 获取 表单数据 表单验证  验证通过提交登陆 处理结果
       this.loginLoading = true
-      const user = this.user
 
-      requset({
-        method: 'POST',
-        url: '/mp/v1_0/authorizations',
-        data: user
-      }).then(res => {
-        console.log(res)
+      login(this.user).then(res => {
+        // console.log(res)
         this.$message({
           message: '登陆成功',
           type: 'success'
         })
         this.loginLoading = false
+        // eslint-disable-next-line handle-callback-err
       }).catch(err => {
-        console.log(err)
         this.$message.error('登陆失败,手机号或验证码错误')
+        // console.log(err)
         this.loginLoading = false
       })
     }
